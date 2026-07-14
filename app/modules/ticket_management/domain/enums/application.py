74a64d1065b1656f0c06afc1b9fe5d2db37a5d5a
MohamedLouayChatti <mohamedlouay.chatti@insat.ucar.tn>
2026-07-14 14:52:14 +0100

    chore: Updating application names with actual business names

--- app/modules/ticket_management/domain/enums/application.py
+++ app/modules/ticket_management/domain/enums/application.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from enum import StrEnum
 
 class Application(StrEnum):
-    APP_1 = "APP_1"
-    APP_2 = "APP_2"
-    APP_3 = "APP_3"
-    APP_4 = "APP_4"
+    FCI = "FCI"
+    COLORIS = "COLORIS"
+    AERO = "AERO"
+    VIO = "VIO"
