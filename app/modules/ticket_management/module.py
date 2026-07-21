f86c192217c204b3a56c750f2f19063883c06464
MohamedLouayChatti <mohamedlouay.chatti@insat.ucar.tn>
2026-07-21 15:45:43 +0100

    chore: Renaming ticket management module endpoints file for convention consistency

--- app/modules/ticket_management/module.py
+++ app/modules/ticket_management/module.py
@@ -1,3 +1,3 @@
-from app.modules.ticket_management.api.router import attachments_router, comments_router, router
+from app.modules.ticket_management.api.routes import attachments_router, comments_router, router
 
 __all__ = ["attachments_router", "comments_router", "router"]
